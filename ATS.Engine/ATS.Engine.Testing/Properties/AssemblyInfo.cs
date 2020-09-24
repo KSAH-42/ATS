eeb416c2a90b23a4903a4e219a18fc46e7fce7fc
KSAH-42 <a.sahnine@netcourrier.com>
2020-09-24 10:46:30 +0200

    Creation of the project

---
+++ ATS.Engine/ATS.Engine.Testing/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle( "ATS.Engine.Testing" )]
+[assembly: AssemblyDescription( "" )]
+[assembly: AssemblyConfiguration( "" )]
+[assembly: AssemblyCompany( "HP Inc." )]
+[assembly: AssemblyProduct( "ATS.Engine.Testing" )]
+[assembly: AssemblyCopyright( "Copyright © HP Inc. 2020" )]
+[assembly: AssemblyTrademark( "" )]
+[assembly: AssemblyCulture( "" )]
+
+[assembly: ComVisible( false )]
+
+[assembly: Guid( "dbed047d-cafc-46fb-a458-8b5e5536bd4c" )]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion( "1.0.0.0" )]
+[assembly: AssemblyFileVersion( "1.0.0.0" )]
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # ATS
+
+I create this project in order to explorer a different to build client API.
+So here, ATS stands for Automatic Trading Software.
+
+This project represent a client api used to managed banking operations like:
+
+* Open account
+* Buy credit card
+* Deactivate a credit card 
+* Query the transaction
+* etc...
+
+I think that the main concept of this project can be resued on others domain.
